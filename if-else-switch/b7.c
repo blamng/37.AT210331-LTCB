22ddbe9880a4c85d7c41c1a169662b58e97d9fd8
blamng <nghienlam084@gmail.com>
2025-03-03 14:29:26 +0000

    Create b7.c

---
+++ if-else-switch/b7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+   double dtb;
+   scanf("%lf", &dtb);
+   if(n < 5)
+       printf("Y");
+   else if(n >= 5 && n < 7)
+       printf("TB");
+   else if(n >= 7 && n < 8)
+       printf("K");
+   else if(n >= 8 && n < 9)
+       printf("G");
+   else
+       printf("XS");
+   return 0;
+}
