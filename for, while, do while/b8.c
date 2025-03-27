8f8001c2864608d9318ad79cafc7d9d258daf52b
blamng <nghienlam084@gmail.com>
2025-03-27 08:23:46 +0000

    Create b8.c

---
+++ for, while, do while/b8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	long long giaithua = 1;
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	for(int i=1; i<=n; i++)
+		giaithua *= i;
+	printf("%d! bang : %lld", n, giaithua);
+}
