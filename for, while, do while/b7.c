d0d42828f748d0063f4d718ff640d5670e058859
blamng <nghienlam084@gmail.com>
2025-03-27 08:15:37 +0000

    Update b7.c

--- for, while, do while/b7.c
+++ for, while, do while/b7.c
@@ -5,10 +5,8 @@ int main()
 	printf("Nhap n: ");
 	scanf("%d", &n);
 	double s;
-	for(int i=1; i<=n; i++)
-	{
-		s += 1.0/i;
-	}
+	for(int i=1; i<=n; i++)	
+		s += 1.0/i;	
 	printf("Tong la: %lf", s);
 	return 0;
 }
